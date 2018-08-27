ffd991fbff185035dd474f438bfb9f33d436435c
DESKTOP-8L88PD5\ccoder <Kingdompin@163.com>
2018-08-27 18:42:16 +0800

    simple array 283

---
+++ src/leetcode/simple/array/MoveZeroes.java
@@ -0,0 +1,81 @@
+package leetcode.simple.array;
+
+import leetcode.utils.ArrayUtils;
+
+/**
+ * @description: 283. 移动零
+ * @link: <a>https://leetcode-cn.com/problems/move-zeroes/description/</a>
+ * @author: guoping wang
+ * @date: 2018/8/27 15:31
+ * @project: cc-leetcode
+ */
+public class MoveZeroes {
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 0, 3, 0};
+        new MoveZeroes().moveZeroes(nums);
+        ArrayUtils.printArray(nums);
+    }
+
+    /**
+     * 冒泡排序思路
+     * 96ms 2.90%
+     * @param nums
+     */
+    public void moveZeroesBubble(int[] nums) {
+        for (int i = nums.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] != 0) {
+                    continue;
+                }
+                nums[j] = nums[j] + nums[j + 1];
+                nums[j + 1] = nums[j] - nums[j + 1];
+                nums[j] = nums[j] - nums[j + 1];
+            }
+        }
+    }
+
+    /**
+     * 个人常规算法：O(n) 挨个查看是否为0，是0的寻找最近的换上
+     * 27ms 14,79%
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                continue;
+            }
+            int count = 0;
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] == 0) {
+                    count++;
+                } else {
+                    break;
+                }
+                if (j == nums.length - 1) {
+                    return;
+                }
+            }
+            nums[i] = nums[i + count];
+            nums[i + count] = 0;
+        }
+    }
+
+    /**
+     * 最快解法：假装额外开辟了数组空间
+     * 2ms 99.97%
+     */
+    public void moveZeroesFast(int[] nums) {
+        // 第二个数组的索引
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // 不等于0的数拷贝至第二数组
+                nums[index++] = nums[i];
+            }
+        }
+        for (int i = index; i < nums.length; i++) {
+            nums[index++] = 0;
+        }
+    }
+}
---
+++ src/leetcode/utils/ArrayUtils.java
@@ -0,0 +1,21 @@
+package leetcode.utils;
+
+/**
+ * @description: 数组工具类
+ * @author: guoping wang
+ * @date: 2018/8/27 16:09
+ * @project: cc-leetcode
+ */
+public class ArrayUtils {
+
+    public static void printArray(int[] nums) {
+        System.out.print("[");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+            if (i != nums.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+}
