78dd31277f7765c4bf9f93795d9a7f283b42073a
DESKTOP-8L88PD5\ccoder <Kingdompin@163.com>
2018-08-28 19:00:16 +0800

    Add bubble sort, insertion sort and selection sort algorithms.

---
+++ src/leetcode/learn/sort/Book.java
@@ -0,0 +1,64 @@
+package leetcode.learn.sort;
+
+import java.util.Arrays;
+
+/**
+ * @description: 书籍类
+ * @author: guoping wang
+ * @date: 2018/8/28 14:01
+ * @project: cc-leetcode
+ */
+public class Book implements Comparable<Book> {
+
+    private String name;
+    private int page;
+    private int price;
+
+    public Book(String name, int page, int price) {
+        this.name = name;
+        this.page = page;
+        this.price = price;
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        if (this.price < o.price) {
+            return -1;
+        }
+        if (this.price > o.price) {
+            return 1;
+        }
+        if (this.page < o.page) {
+            return -1;
+        }
+        if (this.page > o.page) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", page=" + page +
+                ", price=" + price +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Book book1 = new Book("book1", 200, 10);
+        Book book2 = new Book("book2", 230, 30);
+        Book book3 = new Book("book3", 400, 30);
+        Book book4 = new Book("book4", 400, 50);
+        Book[] books = {book2, book3, book1, book4};
+        for (Book book : books) {
+            System.out.println(book);
+        }
+        Arrays.sort(books);
+        System.out.println("排序后：");
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+}
---
+++ src/leetcode/learn/sort/SelectionSort.java
@@ -0,0 +1,35 @@
+package leetcode.learn.sort;
+
+import leetcode.utils.ArrayUtils;
+
+/**
+ * @description: 选择排序
+ * @author: guoping wang
+ * @date: 2018/8/28 15:32
+ * @project: cc-leetcode
+ */
+public class SelectionSort {
+
+    public static int[] sort(int[] array) {
+        // 外层循环控制着每一次数组最小值的寻找范围，也就是定义了起点
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            // 内存循环寻找最小值所在位置
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // 交换位置
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {53, 34, 32, 56, 62, 121, 55};
+        ArrayUtils.printArray(sort(array));
+    }
+}
