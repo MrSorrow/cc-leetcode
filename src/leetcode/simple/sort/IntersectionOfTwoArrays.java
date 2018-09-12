bf34c07d968afdc7b4ec2342369f52495ecf9920
wangguoping <Kingdompin@163.com>
2018-09-12 15:20:07 +0800

    simple sort 349.

--- src/leetcode/simple/sort/IntersectionOfTwoArrays.java
+++ src/leetcode/simple/sort/IntersectionOfTwoArrays.java
@@ -32,7 +32,7 @@ public class IntersectionOfTwoArrays {
         HashSet<Integer> num1Set = new HashSet<>();
         for (int i = 0; i < nums1.length; i++) {
             num1Set.add(nums1[i]);
-        } 
+        }
         for (int i = 0; i < nums2.length; i++) {
             if (num1Set.contains(nums2[i])) {
                 res.add(nums2[i]);
