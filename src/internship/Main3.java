e53b56cc4322a5ab08b687ddc80813d33b473456
DESKTOP-8L88PD5\ccoder <Kingdompin@163.com>
2019-04-08 20:37:49 +0800

    Main3.

--- src/internship/Main3.java
+++ src/internship/Main3.java
@@ -6,29 +6,15 @@ public class Main3 {
 
     public static void main(String[] args)
     {
-
         Scanner input =new Scanner(System.in);
 
         while(input.hasNext())
         {
             HashMap<String, Integer> map = new HashMap<>();
 
-            // int n = input.nextInt();
-            //String[] s = new String[n];
-            //int[] re = new int[n];
-
             StringBuffer rr = new StringBuffer();
             String[] pathN = input.nextLine().split(" ");
 
-
-			/*String[] tt = "/order/sda/asa/bbb".split("/");
-			System.out.println(tt.length);
-
-			for (int i = 0; i < tt.length; i++) {
-				System.out.println(tt[i]);
-			}*/
-
-            // System.out.println(pathN.length);
             for (int i = 1; i < pathN.length; i++) {
 
                 String tmp = pathN[i];
@@ -43,18 +29,14 @@ public class Main3 {
                     map.put(tmp, 1);
                 }
 
-                // System.out.println(map);
-                // System.out.println("as"+c);
-
                 for (int j = 1; j < te.length; j++) {
-                    // String string = te[j];
 
                     if(j == 1) {
                         rr.append("1");
                     } else if (j == te.length - 1) {
                         rr.append("1");
                     } else {
-                        System.out.println("as1: "+c);
+                        //System.out.println("as1: "+c);
                         rr.append(c);
                     }
                 }
@@ -62,13 +44,10 @@ public class Main3 {
                 if(i != pathN.length - 1) {
                     rr.append(" ");
                 }
-
             }
-            System.out.println(rr);
-
-
+            System.out.println(rr.toString());
         }
-
     }
+
 
 }
