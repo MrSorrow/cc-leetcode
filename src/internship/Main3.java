cd0299d6907eeca2dd3945f6bf65ac797f17f41f
DESKTOP-8L88PD5\ccoder <Kingdompin@163.com>
2019-04-08 20:46:34 +0800

    Main3.

--- src/internship/Main3.java
+++ src/internship/Main3.java
@@ -17,23 +17,24 @@ public class Main3 {
 
             for (int i = 1; i < pathN.length; i++) {
 
-                String tmp = pathN[i];
-                String[] te = pathN[i].split("/");
+                // String tmp = pathN[i];
+                // String[] te = pathN[i].split("/");
+                int te = pathN[i].split("/").length;
 
                 int  c = 0;
-                if(map.containsKey(tmp)) {
-                    c = map.get(tmp) + 1;
+                if(map.containsKey(pathN[i])) {
+                    c = map.get(pathN[i]) + 1;
                     map.put(pathN[i], c);
                 } else {
                     c = 1;
-                    map.put(tmp, 1);
+                    map.put(pathN[i], 1);
                 }
 
-                for (int j = 1; j < te.length; j++) {
+                for (int j = 1; j < te; j++) {
 
                     if(j == 1) {
                         rr.append("1");
-                    } else if (j == te.length - 1) {
+                    } else if (j == te - 1) {
                         rr.append("1");
                     } else {
                         //System.out.println("as1: "+c);
@@ -48,6 +49,5 @@ public class Main3 {
             System.out.println(rr.toString());
         }
     }
-
 
 }
