07db30a196de03fb331e8d863694c91cffb2c96a
DESKTOP-8L88PD5\ccoder <Kingdompin@163.com>
2019-04-10 19:44:06 +0800

    simple other .

--- src/internship/Main.java
+++ src/internship/Main.java
@@ -1,41 +1,88 @@
 package internship;
 
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
-        int k = Integer.valueOf(scanner.nextLine());
+        // int[] a = {5,1,2,3};
+        Scanner input = new Scanner(System.in);
 
-        if (line.length() > 2) {
-            String[] strs = line.substring(1, line.length() - 1).split(",");
-            String[] strsCopy = new String[strs.length];
-
-            int index = 0, add = 0;
-            while (strs.length - index >= k) {
-                for (int i = index + k - 1; i >= index; i--) {
-                    strsCopy[add++] = strs[i];
-                }
-                index += k;
-            }
-            if (index < strs.length) {
-                for (int i = index; i < strs.length; i++) {
-                    strsCopy[add++] = strs[i];
+        while (input.hasNext()) {
+            int n = input.nextInt();
+            int m = input.nextInt();
+            int[][] h = new int[n][m];
+            // System.out.println(n+","+m);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    h[i][j] = input.nextInt();
                 }
             }
 
-            StringBuilder sb = new StringBuilder("[");
-            for (int i = 0; i < strsCopy.length; i++) {
-                sb.append(strsCopy[i]);
-                if (i < strsCopy.length - 1) sb.append(",");
-            }
-            sb.append("]");
-            System.out.println(sb.toString());
+            int n1 = input.nextInt();
+            int m1 = input.nextInt();
+            int n2 = input.nextInt();
+            int m2 = input.nextInt();
 
-        } else {
-            System.out.println(line);
+            //System.out.println(n1+","+m1);
+            //System.out.println(n2+","+m2);
+
+            int result = h[n2][m2];
+            int count = 0;
+
+            Queue<int[]> queue = new LinkedList<>();
+            int[] start = {n1, m1};
+            queue.offer(start);
+
+            while (!queue.isEmpty()) {
+
+                int size = queue.size();
+
+                for (int i = 1; i <= size; i++) {
+                    int[] tmp = queue.poll();
+                    int tn = tmp[0];
+                    int tm = tmp[1];
+
+                    if(tn - 1 >= 0 && h[tn - 1][tm] > h[tn][tm]) {
+                        if(h[tn - 1][tm] == result) {
+                            count++;
+                        } else if(h[tn - 1][tm] < result) {
+                            int[] step = {tn - 1, tm};
+                            queue.offer(step);
+                        }
+                    }
+
+                    if(tn + 1 <= n - 1 && h[tn + 1][tm] > h[tn][tm]) {
+                        if(h[tn + 1][tm] == result) {
+                            count++;
+                        } else if(h[tn + 1][tm] < result) {
+                            int[] step = {tn + 1, tm};
+                            queue.offer(step);
+                        }
+                    }
+
+                    if(tm - 1 >= 0 && h[tn][tm - 1] > h[tn][tm]) {
+                        if(h[tn][tm - 1] == result) {
+                            count++;
+                        } else if(h[tn][tm - 1] < result) {
+                            int[] step = {tn, tm - 1};
+                            queue.offer(step);
+                        }
+                    }
+
+                    if(tm + 1 <= m - 1 && h[tn][tm + 1] > h[tn][tm]) {
+                        if(h[tn][tm + 1] == result) {
+                            count++;
+                        } else if(h[tn][tm + 1] < result) {
+                            int[] step = {tn, tm + 1};
+                            queue.offer(step);
+                        }
+                    }
+                }
+            }
+            System.out.println(count);
         }
     }
 }
